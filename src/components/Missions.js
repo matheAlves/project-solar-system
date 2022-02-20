fb53126b31115876ed8da281aad949d023372a48
Matheus S Alves <matheus.souza.alves@gmail.com>
2022-02-20 18:48:35 -0300

    requisito 7 :heavy_check_mark:

---
+++ src/components/Missions.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Missions extends Component {
+  render() {
+    return (
+      <div data-testid="missions">
+        <p>alá</p>
+      </div>
+    );
+  }
+}
+
+export default Missions;
