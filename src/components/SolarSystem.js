db13db9da68a33eea95a0cdc366d4e7f2722262f
Matheus S Alves <matheus.souza.alves@gmail.com>
2022-02-20 17:47:32 -0300

    requisito 4 :heavy_check_mark:

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Title from './Title';
 
 class SolarSystem extends Component {
   render() {
     return (
       <div data-testid="solar-system">
-        <span>hello world</span>
+        <Title headline="Planetas" />
       </div>
     );
   }
